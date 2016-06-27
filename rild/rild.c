xianyo/android_hardware_ril

ENGR00232938 Integrate ZTE 3G modem.
To let various modems choose its own libreference-ril.so. Need to
identify modem in rild.c using methods in runtime_port.c. These
methods are also shared in reference-ril/. So we make runtime-like
thing as a shared lib.

Signed-off-by: Shi Wenjun <b42754@freescale.com>

rild/rild.c
@@ -34,11 +34,14 @@
 
 #include <private/android_filesystem_config.h>
 #include "hardware/qemu_pipe.h"
+#include <runtime/runtime.h>
 
 #define LIB_PATH_PROPERTY   "rild.libpath"
 #define LIB_ARGS_PROPERTY   "rild.libargs"
 #define MAX_LIB_ARGS        16
-
+#define MAX_POLL_DEVICE_CNT 8
+#define REFERENCE_RIL_DEF_PATH "/system/lib/libreference-ril.so"
+#define REFERENCE_RIL_ZTE_PATH "/system/lib/libreference-ril-zte.so"
 static void usage(const char *argv0)
 {
     fprintf(stderr, "Usage: %s -l <ril impl library> [-- <args for impl library>]\n", argv0);
@@ -62,6 +65,7 @@ static struct RIL_Env s_rilEnv = {
     RIL_onUnsolicitedResponse,
     RIL_requestTimedCallback
 };
+static int s_poll_device_cnt = 0;
 
 extern void RIL_startEventLoop();
 
@@ -106,7 +110,7 @@ int main(int argc, char **argv)
     const RIL_RadioFunctions *funcs;
     char libPath[PROPERTY_VALUE_MAX];
     unsigned char hasLibArgs = 0;
-
+    int modem_type = UNKNOWN_MODEM;
     int i;
 
     umask(S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
@@ -121,6 +125,38 @@ int main(int argc, char **argv)
         } else {
             usage(argv[0]);
         }
+    }
+
+    //Wait for device ready.
+    if (rilLibPath == NULL) {
+		while(UNKNOWN_MODEM == modem_type){
+		    modem_type = runtime_3g_port_type();
+		    LOGD("Couldn't find proper modem, retrying...");
+		    s_poll_device_cnt++;
+		    if (s_poll_device_cnt > MAX_POLL_DEVICE_CNT){
+				/*
+				*Maybe no device right now, start to monitor
+				*hotplug event later.
+				*/
+				start_uevent_monitor();
+				goto done;
+		    }
+		    sleep(5);
+		}
+    }
+
+    start_uevent_monitor();
+
+    switch (modem_type){
+		case ZTE_MODEM:
+		rilLibPath = REFERENCE_RIL_ZTE_PATH;
+		break;
+
+		case HUAWEI_MODEM:
+		case AMAZON_MODEM:
+		default:
+		rilLibPath = REFERENCE_RIL_DEF_PATH;
+		break;
     }
 
     if (rilLibPath == NULL) {
@@ -271,7 +307,6 @@ OpenLib:
 
     // Make sure there's a reasonable argv[0]
     rilArgv[0] = argv[0];
-
     funcs = rilInit(&s_rilEnv, argc, rilArgv);
 
     RIL_register(funcs);

technexion-android/platform_hardware_ril

Change-Id: Ib86cecba95a4c919d672d6bb3929abeb059d6aed
Conflicts:
	rild/rild.c

rild/rild.c
@@ -37,13 +37,17 @@
 
 #include <private/android_filesystem_config.h>
 #include "hardware/qemu_pipe.h"
+#include <runtime/runtime.h>
 
 #define LIB_PATH_PROPERTY   "rild.libpath"
 #define LIB_ARGS_PROPERTY   "rild.libargs"
 #define MAX_LIB_ARGS        16
 #define MAX_CAP_NUM         (CAP_TO_INDEX(CAP_LAST_CAP) + 1)
-
-static void usage(const char *argv0) {
+#define MAX_POLL_DEVICE_CNT 8
+#define REFERENCE_RIL_DEF_PATH "/system/lib/libreference-ril.so"
+#define REFERENCE_RIL_ZTE_PATH "/system/lib/libreference-ril-zte.so"
+static void usage(const char *argv0)
+{
     fprintf(stderr, "Usage: %s -l <ril impl library> [-- <args for impl library>]\n", argv0);
     exit(EXIT_FAILURE);
 }
@@ -77,6 +81,7 @@ static struct RIL_Env s_rilEnv = {
     RIL_onUnsolicitedResponse,
     RIL_requestTimedCallback
 };
+static int s_poll_device_cnt = 0;
 
 extern void RIL_startEventLoop();
 
@@ -147,7 +152,7 @@ int main(int argc, char **argv) {
     const RIL_RadioFunctions *funcs;
     char libPath[PROPERTY_VALUE_MAX];
     unsigned char hasLibArgs = 0;
-
+    int modem_type = UNKNOWN_MODEM;
     int i;
     const char *clientId = NULL;
     RLOGD("**RIL Daemon Started**");
@@ -178,6 +183,37 @@ int main(int argc, char **argv) {
     }
     if (strncmp(clientId, "0", MAX_CLIENT_ID_LENGTH)) {
         RIL_setRilSocketName(strncat(rild, clientId, MAX_SOCKET_NAME_LENGTH));
+    }
+    //Wait for device ready.
+    if (rilLibPath == NULL) {
+		while(UNKNOWN_MODEM == modem_type){
+		    modem_type = runtime_3g_port_type();
+		    ALOGD("Couldn't find proper modem, retrying...");
+		    s_poll_device_cnt++;
+		    if (s_poll_device_cnt > MAX_POLL_DEVICE_CNT){
+				/*
+				*Maybe no device right now, start to monitor
+				*hotplug event later.
+				*/
+				start_uevent_monitor();
+				goto done;
+		    }
+		    sleep(5);
+		}
+    }
+
+    start_uevent_monitor();
+
+    switch (modem_type){
+		case ZTE_MODEM:
+		rilLibPath = REFERENCE_RIL_ZTE_PATH;
+		break;
+
+		case HUAWEI_MODEM:
+		case AMAZON_MODEM:
+		default:
+		rilLibPath = REFERENCE_RIL_DEF_PATH;
+		break;
     }
 
     if (rilLibPath == NULL) {
@@ -345,7 +381,6 @@ OpenLib:
 
     // Make sure there's a reasonable argv[0]
     rilArgv[0] = argv[0];
-
     funcs = rilInit(&s_rilEnv, argc, rilArgv);
     RLOGD("RIL_Init rilInit completed");
 